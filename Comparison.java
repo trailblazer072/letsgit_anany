98f2a9f6eb120aca2d5dab38d1e62b6ee252fb1d
Anany Sharma <ananysharma04@gmail.com>
2023-09-17 14:32:54 +0530

    Adding new Comparison.java

---
+++ Comparison.java
@@ -0,0 +1,14 @@
+public class Comparison{
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        System.out.println(a*b);
+        System.out.println(a%b);
+        if(a>b){
+        System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
